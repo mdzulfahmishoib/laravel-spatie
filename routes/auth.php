3679a1df3b38830c43f09bee7f20b81607e489a4
fahmi.shoib <mdzulfahmishoib@gmail.com>
2025-11-26 18:14:40 +0700

    membatasi percobaan login 5 kali

--- routes/auth.php
+++ routes/auth.php
@@ -8,9 +8,7 @@ Route::middleware('guest')->group(function () {
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
                 ->name('login');
 
-    // batasi maksimal 3 kali percobaan login per menit
-    Route::post('login', [AuthenticatedSessionController::class, 'store'])
-        ->middleware('throttle:3,1');
+    Route::post('login', [AuthenticatedSessionController::class, 'store']);
 });
 
 Route::middleware('auth')->group(function () {
